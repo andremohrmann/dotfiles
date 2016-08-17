1010853a22ed5d631651b0f6d8faa1314dbfaca6
andremohrmann <andre.mohrmann@online.de>
2016-08-17 14:54:37 +0200

    Beautified 'l' alias

--- .zsh/zshrc.d/main.zsh
+++ .zsh/zshrc.d/main.zsh
@@ -99,7 +99,7 @@ export LS_OPTIONS='--color=auto'
 eval "`dircolors`"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
+alias l='ls $LS_OPTIONS -lAhF'
 
 # grep beautification
 export GREP_OPTIONS="--colour=auto"
