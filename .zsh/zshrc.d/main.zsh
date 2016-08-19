f20f39c89ac84dcac24d659d724e1d9dbda5e074
andremohrmann <andre.mohrmann@online.de>
2016-08-19 17:24:35 +0200

    Do not ignore history duplicates

--- .zsh/zshrc.d/main.zsh
+++ .zsh/zshrc.d/main.zsh
@@ -36,7 +36,7 @@ esac
 setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
-setopt hist_ignore_dups
+#setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
